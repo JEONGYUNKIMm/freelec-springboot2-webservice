000b7b83caa573d3f117db9d6dddc1b095745e47
JEONGYUNKIMm <101933990+JEONGYUNKIMm@users.noreply.github.com>
2022-03-26 00:33:55 +0900

    chapter2

--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -10,12 +10,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
+import static org.hamcrest.Matchers.is;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, secure = false)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -25,5 +28,13 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount))).andExpect(status().isOk()).andExpect(jsonPath("$.name", is(name))).andExpect(jsonPath("$.amount", is(amount)));
     }
 }
